7bf64978a0684da0151e503c610a0ebc512c4cad
Slayer <slayerpro27@gmail.com>
2024-10-03 18:13:15 +0300

    Small refactoring with small improvements

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -1,6 +1,20 @@
 import re
 
 
+headers = {
+    'Accept': '*/*',
+    'Accept-Language': 'ru-RU,ru;q=0.9',
+    'Connection': 'keep-alive',
+    'Origin': 'https://ago-wallet.hexacore.io',
+    'Referer': 'https://ago-wallet.hexacore.io/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'no-cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'Sec-Ch-Ua-Mobile': '?1',
+    'Sec-Ch-Ua-Platform': '"Android"',
+}
+
+
 def get_sec_ch_ua(user_agent):
     pattern = r'(Chrome|Chromium)\/(\d+)\.(\d+)\.(\d+)\.(\d+)'
 
@@ -18,19 +32,3 @@ def get_sec_ch_ua(user_agent):
         return {'Sec-Ch-Ua': sec_ch_ua}
     else:
         return {}
-
-
-headers = {
-    'Accept': '*/*',
-    'Accept-Language': 'ru-RU,ru;q=0.9',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/json',
-    'Origin': 'https://ago-wallet.hexacore.io',
-    'Referer': 'https://ago-wallet.hexacore.io/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'no-cors',
-    'Sec-Fetch-Site': 'cross-site',
-    'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-    'Sec-Ch-Ua-Mobile': '?1',
-    'Sec-Ch-Ua-Platform': '"Android"',
-}
